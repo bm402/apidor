816edb725f8ed1011a89d6ed8353326ea07f4ddf
bncrypted <bncrypted@gmail.com>
2020-10-06 16:07:06 +0100

    reads sample API definition file

--- pkg/definition/definition.go
+++ pkg/definition/definition.go
@@ -9,42 +9,42 @@ import (
 
 // Definition is a full model of the YAML definition file
 type Definition struct {
-	baseURI     string                   `yaml:"base"`
-	authDetails AuthDetails              `yaml:"auth"`
-	vars        map[string][]interface{} `yaml:"vars"`
-	api         APIDetails               `yaml:"api"`
+	BaseURI     string           `yaml:"base"`
+	AuthDetails AuthDetails      `yaml:"auth"`
+	Vars        map[string][]int `yaml:"vars"`
+	API         APIDetails       `yaml:"api"`
 }
 
 // AuthDetails is a model of the authentication header details
 type AuthDetails struct {
-	headerName        string `yaml:"header_name"`
-	headerValuePrefix string `yaml:"header_value_prefix"`
-	high              string `yaml:"high_privileged_access_token"`
-	low               string `yaml:"low_privileged_access_token"`
+	HeaderName        string `yaml:"header_name"`
+	HeaderValuePrefix string `yaml:"header_value_prefix"`
+	High              string `yaml:"high_privileged_access_token"`
+	Low               string `yaml:"low_privileged_access_token"`
 }
 
 // APIDetails is a model for the API details
 type APIDetails struct {
-	globalMethods []string                   `yaml:"methods"`
-	globalHeaders map[string]string          `yaml:"headers"`
-	endpoints     map[string]EndpointDetails `yaml:"endpoints"`
+	GlobalMethods []string                   `yaml:"methods"`
+	GlobalHeaders map[string]string          `yaml:"headers"`
+	Endpoints     map[string]EndpointDetails `yaml:"endpoints"`
 }
 
 // EndpointDetails is a model for an endpoint in the API
 type EndpointDetails struct {
-	operation     string                 `yaml:"operation"`
-	method        string                 `yaml:"method"`
-	contentType   string                 `yaml:"content_type"`
-	headers       map[string]string      `yaml:"headers"`
-	requestParams map[string]string      `yaml:"request_params"`
-	bodyParams    map[string]interface{} `yaml:"body_params"`
+	Operation     string                 `yaml:"operation"`
+	Method        string                 `yaml:"method"`
+	ContentType   string                 `yaml:"content_type"`
+	Headers       map[string]string      `yaml:"headers"`
+	RequestParams map[string]string      `yaml:"request_params"`
+	BodyParams    map[string]interface{} `yaml:"body_params"`
 }
 
 // ReadDefinition reads the API definition from the given YAML file
 func ReadDefinition(path string) Definition {
 	buf, err := ioutil.ReadFile(path)
 	if err != nil {
-		fmt.Println("Error reading definition file")
+		fmt.Println("Error reading definition file: ", err.Error())
 	}
 
 	var definition Definition
