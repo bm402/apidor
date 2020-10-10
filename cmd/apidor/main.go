0875aab383ad593554c1e00bcb6a9d72b6212f97
bncrypted <bncrypted@gmail.com>
2020-10-10 19:47:51 +0100

    added proxy and cert flags

--- cmd/apidor/main.go
+++ cmd/apidor/main.go
@@ -7,27 +7,43 @@ import (
 	"github.com/bncrypted/apidor/internal/apidor/logger"
 	"github.com/bncrypted/apidor/internal/apidor/workflow"
 	"github.com/bncrypted/apidor/pkg/definition"
+	"github.com/bncrypted/apidor/pkg/http"
 )
 
 func main() {
-	flags := logger.Flags{
-		DefinitionFile: flag.String("d", "definitions/sample.yml", "Path to the API definition YAML file"),
-		LogFile:        flag.String("o", "", "Log file name"),
-		IsDebug:        flag.Bool("debug", false, "Specifies whether to use debugging mode for verbose output"),
-	}
+	definitionFile := flag.String("d", "definitions/sample.yml", "Path to the API definition YAML file")
+	localCertFile := flag.String("cert", "", "Path to a local certificate authority file")
+	logFile := flag.String("o", "", "Log file name")
+	proxyURI := flag.String("proxy", "", "Gives a URI to proxy HTTP traffic through")
+	isDebug := flag.Bool("debug", false, "Specifies whether to use debugging mode for verbose output")
 	flag.Parse()
 
-	logger.Init(flags)
+	httpFlags := http.Flags{
+		ProxyURI:      *proxyURI,
+		LocalCertFile: *localCertFile,
+	}
+
+	http.Init(httpFlags)
+
+	loggerFlags := logger.Flags{
+		DefinitionFile: *definitionFile,
+		LocalCertFile:  *localCertFile,
+		LogFile:        *logFile,
+		ProxyURI:       *proxyURI,
+		IsDebug:        *isDebug,
+	}
+
+	logger.Init(loggerFlags)
 	defer logger.Close()
 	logger.Logo()
 
-	definition, err := definition.Read(*flags.DefinitionFile)
+	definition, err := definition.Read(*definitionFile)
 	if err != nil {
 		logger.Fatal(err.Error())
 		os.Exit(1)
 	}
 
-	logger.RunInfo(definition.BaseURI, len(definition.API.Endpoints), flags)
+	logger.RunInfo(definition.BaseURI, len(definition.API.Endpoints), loggerFlags)
 	logger.Starting()
 
 	workflow.Run(definition, workflow.Flags{})
