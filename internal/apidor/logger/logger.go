218a9d899d586f8c0c342d9727c69a16d48b737c
bncrypted <bncrypted@gmail.com>
2020-11-09 19:45:49 +0000

    added body param normalisation

--- internal/apidor/logger/logger.go
+++ internal/apidor/logger/logger.go
@@ -118,8 +118,8 @@ func Finished() {
 }
 
 // TestPrefix is a logger function that prints the endpoint and the test name that is taking place
-func TestPrefix(requestID int, endpoint string, testName string) {
-	prefix := "[" + strconv.Itoa(requestID) + "][" + endpoint + "][" + testName + "] "
+func TestPrefix(requestID int, method string, endpoint string, testName string) {
+	prefix := "[" + strconv.Itoa(requestID) + "][" + method + "[]" + endpoint + "][" + testName + "] "
 	if isDebug {
 		writeln(prefix)
 		writeln("")
